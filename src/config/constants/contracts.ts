33e0b64864406ec1208c1369b26d27f1fcccb293
SafaOzince <safabaturalpozince@gmail.com>
2022-04-20 15:20:04 +0300

    contractupdate

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -1,15 +1,15 @@
 export default {
   cake: {
     56: '0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6',
-    97: '',
+    97: '0x43E471023D44F4360d1Fd5D0E4504C841e33C45B', //token contract
   },
   masterChef: {
     56: '0xe70E9185F5ea7Ba3C5d63705784D8563017f2E57',
-    97: '',
+    97: '0x7c83d3f6f1cb5710e6B52fD51c848317fAB93706',
   },
   wbnb: {
     56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
-    97: '',
+    97: '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd',
   },
   lottery: {
     56: '',
@@ -25,6 +25,6 @@ export default {
   },
   busd: {
     56: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-    97: '',
+    97: '0x78867BbEeF44f2326bF8DDd1941a4439382EF2A7',
   },
 }
